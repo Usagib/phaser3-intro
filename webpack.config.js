332262b33eddb3ae7988ddf2020e4b39de277bee
Eduardo Bonilla <eduardobonillac@gmail.com>
2020-06-15 20:42:58 -0500

    tutorial finished

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpe?g|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ],
+  },
+};
